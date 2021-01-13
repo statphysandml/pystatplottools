9f1be66123a5fc4fadb82eb027e9df63b986ed26
kades <kades@thphys.uni-heidelberg.de>
2021-01-13 21:49:14 +0100

    update

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-# python setup.py sdist
-# pip install -e .
-
-# https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
-
-
 from distutils.core import setup
 
 setup(name='pystatplottools',
       version='0.1',
-      description='Python modules for performing simple statistics and plotting routines',
+      description='Python modules for performing simple statistics and plotting routines.',
       author='Lukas Kades',
       author_email='lukaskades@googlemail.com',
+      url='https://github.com/statphysandml/pystatplottools',
       packages=['pystatplottools',
                 'pystatplottools.distributions',
-                'pystatplottools.expectation_values'
+                'pystatplottools.expectation_values',
                 'pystatplottools.pdf_env',
                 'pystatplottools.plotting',
                 'pystatplottools.pytorch_data_generation',

6f8653eaaccb87c5dba4b701d37bf4394d8107a9
lukaskades <lukaskades@gmail.com>
2021-01-13 19:08:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@
 
 # https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 from distutils.core import setup
 
@@ -13,12 +18,15 @@ setup(name='pystatplottools',
       description='Python modules for performing simple statistics and plotting routines',
       author='Lukas Kades',
       author_email='lukaskades@googlemail.com',
+      url='https://github.com/statphysandml/pystatplottools',
       packages=['pystatplottools',
                 'pystatplottools.distributions',
-                'pystatplottools.expectation_values'
+                'pystatplottools.expectation_values',
                 'pystatplottools.pdf_env',
                 'pystatplottools.plotting',
                 'pystatplottools.pytorch_data_generation',
                 'pystatplottools.utils',
-                'pystatplottools.visualization']
+                'pystatplottools.visualization'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
      )
